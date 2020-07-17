cf8e4381e907395726caebb8155970cd831cbf26
Kalley Wilkerson <kalleywra@gmail.com>
2020-07-17 14:26:08 -0300

    Project cleaned to start from ground'

---
+++ src/main/java/com/kalley/social/sellit/ui/MainView.java
@@ -0,0 +1,15 @@
+package com.kalley.social.sellit.ui;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class MainView extends VerticalLayout {
+
+	/**
+	 * Creates a new View to the project.
+	 */
+    public MainView() {
+    }
+
+}
